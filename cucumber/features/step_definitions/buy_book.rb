b5449e2a5d800af7f9424ed24488ff79a3b1b0fa
Stephen Samuel <stephensamuel@Stephens-MacBook-Pro.local>
2018-08-01 15:15:26 +0200

    refactor to use page objects

--- cucumber/features/step_definitions/buy_book.rb
+++ cucumber/features/step_definitions/buy_book.rb
@@ -2,11 +2,11 @@ require 'selenium-webdriver'
 require "watir"
 
 Given /I open "(.*?)"/ do |site|
-	openSite(site)
+	@home.openSite (site)
 end
 
 When /I search for "(.*?)"/ do |keyword|
-	searchByKeyword(keyword)
+	@page.searchByKeyword(keyword)
 end
 
 And /I open the first book/ do
@@ -20,66 +20,16 @@ And /I select the paperback edition/ do
 end
 
 And /I add the first book to shopping cart/ do
-	getCurrentItemTitle
-	addToShoppingCart
+	@itemdetails.getCurrentItemTitle
+	@itemdetails.addToShoppingCart
 end
 
 And /I will go to the cart/ do
-	goToMyCart
+	@page.goToMyCart
 end
 
 Then /I should see the book in my shopping cart/ do
 	bookName = @driver.find_element :xpath => '//*[@id="activeCartViewForm"]/div[2]/div/div[4]/div/div[1]/div/div/div[2]/ul/li[1]/span/a/span'
-	 @bookTitle.include?(bookName.text)
+	 $bookTitle.include?(bookName.text)
 	 @driver.save_screenshot ("screenshot.png")
-end
-
-
-def openSite(site)
-		@driver.get site
-	end
-
-def searchByKeyword(keyword)
-	searchKeyword = findElementBy("id", "twotabsearchtextbox")
-	searchKeyword.clear
-	searchKeyword.send_keys keyword
-	clickElementBy("class", "nav-search-submit")
-end
-
-def getCurrentItemTitle
-	@bookTitle = findElementBy("id", "productTitle").text
-end
-
-def addToShoppingCart
-	clickElementBy("name", "submit.add-to-cart")
-end
-
-def goToMyCart
-	clickElementBy("id", "hlb-view-cart")
-end
-
-def findElementBy (type, value)
-    if type=="class"
-        element=@driver.find_element :class => value
-    elsif type=="css"
-        element=@driver.find_element :css => value
-    elsif type=="id"
-        element=@driver.find_element :id => value
-    elsif type=="link"
-        element=@driver.find_element :link => value
-    elsif type=="name"
-        element=@driver.find_element :name => value
-    elsif type=="partial_link"
-        element=@driver.find_element :partial_link_text => value
-    elsif type=="tag"
-        element=@driver.find_element :tag_name => value
-    elsif type=="xpath"
-        element=@driver.find_element :xpath => value
-    else
-        p "incorrect selector type"
-    end
-end
-
-def clickElementBy (type, value)
-    findElementBy(type, value).click
 end
--- cucumber/features/support/env.rb
+++ cucumber/features/support/env.rb
@@ -1,8 +1,16 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
 Before do
     @driver = Selenium::WebDriver.for :chrome
 		@driver.manage.timeouts.implicit_wait = 30
 		@driver.manage.timeouts.script_timeout = 30
 		@driver.manage.timeouts.page_load = 30
+		@searchresult=SearchResult.new(@driver)
+		@home=Home.new(@driver)
+		@login=Login.new(@driver)
+		@page=Page.new(@driver)
+		@itemdetails=ItemDetails.new(@driver)
 end
 
 After do
