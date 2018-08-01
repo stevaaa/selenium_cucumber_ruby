7e019dff378fd3918fcd19753b89d0b5e4b50817
Stephen Samuel <stephensamuel@Stephens-MacBook-Pro.local>
2018-08-01 14:30:08 +0200

    added screenshot functionality

--- cucumber/features/step_definitions/buy_book.rb
+++ cucumber/features/step_definitions/buy_book.rb
@@ -25,13 +25,13 @@ And /I add the first book to shopping cart/ do
 end
 
 And /I will go to the cart/ do
-	element = @driver.find_element :id => "hlb-view-cart"
-	element.click
+	goToMyCart
 end
 
 Then /I should see the book in my shopping cart/ do
 	bookName = @driver.find_element :xpath => '//*[@id="activeCartViewForm"]/div[2]/div/div[4]/div/div[1]/div/div/div[2]/ul/li[1]/span/a/span'
 	 @bookTitle.include?(bookName.text)
+	 @driver.save_screenshot ("screenshot.png")
 end
 
 
@@ -40,18 +40,46 @@ def openSite(site)
 	end
 
 def searchByKeyword(keyword)
-	searchKeyword = @driver.find_element :id => "twotabsearchtextbox"
+	searchKeyword = findElementBy("id", "twotabsearchtextbox")
 	searchKeyword.clear
 	searchKeyword.send_keys keyword
-	element = @driver.find_element :class =>"nav-search-submit"
-	element.click
+	clickElementBy("class", "nav-search-submit")
 end
 
 def getCurrentItemTitle
-	@bookTitle = @driver.find_element(:id => "productTitle").text
+	@bookTitle = findElementBy("id", "productTitle").text
 end
 
 def addToShoppingCart
-	element = @driver.find_element :name => "submit.add-to-cart"
-	element.click
+	clickElementBy("name", "submit.add-to-cart")
+end
+
+def goToMyCart
+	clickElementBy("id", "hlb-view-cart")
+end
+
+def findElementBy (type, value)
+    if type=="class"
+        element=@driver.find_element :class => value
+    elsif type=="css"
+        element=@driver.find_element :css => value
+    elsif type=="id"
+        element=@driver.find_element :id => value
+    elsif type=="link"
+        element=@driver.find_element :link => value
+    elsif type=="name"
+        element=@driver.find_element :name => value
+    elsif type=="partial_link"
+        element=@driver.find_element :partial_link_text => value
+    elsif type=="tag"
+        element=@driver.find_element :tag_name => value
+    elsif type=="xpath"
+        element=@driver.find_element :xpath => value
+    else
+        p "incorrect selector type"
+    end
+end
+
+def clickElementBy (type, value)
+    findElementBy(type, value).click
 end
